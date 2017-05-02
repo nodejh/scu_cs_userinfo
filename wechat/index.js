6bdc00ee075046c7ccd6cb22f29c6b05168de423
nodejh <jianghangscu@gmail.com>
2017-05-03 04:17:26 +0800

    调试微信程序

--- wechat/index.js
+++ wechat/index.js
@@ -30,11 +30,12 @@ app.use(views(path.join(__dirname, './views'), {
 
 
 app.use(wechat({
-  token: 'token',
-  appid: 'appid',
-  encodingAESKey: 'encodinAESKey',
-  checkSignature: true, // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+  token: config.wechat.token,
+  appid: config.wechat.token.appid,
+  encodingAESKey: config.wechat.encodingAESKey,
+  checkSignature: true,
 }).middleware(async (message) => {
+  console.log('message: ', message);
   // 微信输入信息就是这个 message
   if (message.FromUserName === 'diaosi') {
     // 回复屌丝(普通回复)
