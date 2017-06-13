127529d3c362a6bf03d388c0016a5a9cd35b79e6
nodejh <jianghangscu@gmail.com>
2017-06-13 21:32:25 +0800

    完成解绑

--- wechat/controllers/account.js
+++ wechat/controllers/account.js
@@ -28,11 +28,11 @@ const unbind = async (openId) => {
   const res = { success: false, message: '解绑失败，请重试' };
   try {
     const sqlSelect = 'select * from users where open_id = ?';
-    console.log('openId: ', openId);
     const users = await query(sqlSelect, [openId]);
-    console.log('users: ', users);
     const sqlBackup = 'insert into users_backup set ?';
-    await query(sqlBackup, [users[0]]);
+    const user = users[0];
+    user.datetime = new Date();
+    await query(sqlBackup, [user]);
     const sqlDelete = 'delete from users where open_id = ?';
     await query(sqlDelete, [openId]);
     res.success = true;
@@ -67,6 +67,7 @@ const bind = async (openId, number, password) => {
       major: info.major,
       grade: info.grade,
       status: 0,
+      datetime: new Date(),
     };
     await query(sql, [data]);
     res.success = true;
