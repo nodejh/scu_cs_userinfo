58845a4e224e200b668ae169178c9d06d209be3e
nodejh <jianghangscu@gmail.com>
2017-06-14 20:57:48 +0800

    微信消息长度限制

--- wechat/controllers/wechat.js
+++ wechat/controllers/wechat.js
@@ -84,7 +84,7 @@ const handle = {
       // eslint-disable-next-line
       console.log('text: ', text.length);
       // return '消息过长，无法显示 T_T';
-      return text.length > 1024 ? `${text.substring(0, 1024)}...` : text;
+      return text.length > 1024 ? `${text.substring(0, 1024)}\n太长显示不了啦` : text;
     } catch (e) {
       return e.message;
     }
