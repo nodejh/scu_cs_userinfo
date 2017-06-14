892da5aaf63945bb66ae17228cae7bbc22b4e71b
nodejh <jianghangscu@gmail.com>
2017-06-14 09:15:13 +0800

    编辑信息

--- wechat/controllers/wechat.js
+++ wechat/controllers/wechat.js
@@ -10,7 +10,7 @@ const handle = {
       // 没有绑定
       return constants.WECHAT_NOT_BIND;
     }
-    return `点击此链接编辑个人信息：<a href="http://115.159.65.172/mWechat/info?openId=${openId}>http://115.159.65.172/mWechat/info</a>`;
+    return `点击此链接编辑个人信息：<a href="http://115.159.65.172/mWechat/info?openId=${openId}">http://115.159.65.172/mWechat/info</a>`;
   },
   2: openId => openId,
   // 本学期成绩查询
