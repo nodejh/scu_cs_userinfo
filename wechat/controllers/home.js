184531015de418336b938082e1d45d31de464283
王婷Queenie <wangting@bestwise.cc>
2017-06-28 16:39:18 +0800

    微信认证

--- wechat/controllers/home.js
+++ wechat/controllers/home.js
@@ -1,3 +1,11 @@
+const crypto = require('crypto');
+function sha1(str) {
+  const md5sum = crypto.createHash('sha1');
+  md5sum.update(str);
+  const ciphertext = md5sum.digest('hex');
+  return ciphertext;
+}
+
 const indexPage = async (ctx) => {
   const data = ctx.request.query;
   console.log('Request URL: ', data);
@@ -5,28 +13,24 @@ const indexPage = async (ctx) => {
   const timestamp = data.timestamp;
   const nonce = data.nonce;
   const echostr = data.echostr;
-  const token='scu_cs';
+  const token = 'scu_cs';
   console.log('timestamp: ', timestamp);
   console.log('nonce: ', nonce);
   console.log('signature: ', signature);
     // 将 token/timestamp/nonce 三个参数进行字典序排序
-   const tmpArr = [token, timestamp, nonce];
-    const tmpStr = sha1(tmpArr.sort().join(''));
-    console.log('Sha1 String: ', tmpStr);
+  const tmpArr = [token, timestamp, nonce];
+  const tmpStr = sha1(tmpArr.sort().join(''));
+  console.log('Sha1 String: ', tmpStr);
     // 验证排序并加密后的字符串与 signature 是否相等
-    if (tmpStr === signature) {
+  if (tmpStr === signature) {
         // 原样返回echostr参数内容
-        res.end(echostr);
-        console.log('Check Success');
-    } else {
-        res.end('failed');
-        console.log('Check Failed');
-    }
-  const title = 'Home Page';
-
+    ctx.body = echostr;
+    console.log('Check Success');
+  } else {
+    ctx.body = 'failed';
+    console.log('Check Failed');
+  }
 };
-
-
 module.exports = {
   indexPage,
 };
